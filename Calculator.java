252392afd97114f027c605d77656b6079ca49a50
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-03-25 11:15:40 -0400

    calculator completed; not sure if there is a way to simpify the reading of operators

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,47 @@
+import java.util.*;
+import java.io.*;
 
+public class Calculator {
+  public static double eval(String s) {
+    Scanner in = new Scanner(s);
+    @SuppressWarnings("unchecked")
+    MyDeque<Double> stack = new MyDeque();
+    String cur = "";
+    while (in.hasNext()) {
+      cur = in.next();
+      if (cur.equals("+")) {
+        Double val2 = stack.removeLast();
+        Double val1 = stack.removeLast();
+        stack.addLast(val1+val2);
+      }
+      else if (cur.equals("-")) {
+        Double val2 = stack.removeLast();
+        Double val1 = stack.removeLast();
+        stack.addLast(val1-val2);
+      }
+      else if (cur.equals("*")) {
+        Double val2 = stack.removeLast();
+        Double val1 = stack.removeLast();
+        stack.addLast(val1*val2);
+      }
+      else if (cur.equals("/")) {
+        Double val2 = stack.removeLast();
+        Double val1 = stack.removeLast();
+        stack.addLast(val1/val2);
+      }
+      else if (cur.equals("%")) {
+        Double val2 = stack.removeLast();
+        Double val1 = stack.removeLast();
+        stack.addLast(val1%val2);
+      }
+      else {
+        stack.addLast(Double.parseDouble(cur));
+      }
+    }
+    return stack.getLast();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(eval("1 42 - 3 4 5 + * +"));
+  }
+}
